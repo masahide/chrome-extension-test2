6a9b434c16fada06256bbf1af52cfe24945fee44
Masahide Yamasaki <masahide.y@gmail.com>
2024-02-11 00:51:52 +0900

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,47 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { crx, defineManifest, } from '@crxjs/vite-plugin';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+//import { viteStaticCopy } from "vite-plugin-static-copy"; // ↓ この行を追加
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: "Counter",
+  name: "svelte chrome extension test",
+  description: "A test simple counter extension",
   version: "1.0.0",
-  permissions: [],
   action: {
-    default_popup: "index.html",
+    //default_popup: "src/popup/index.html",
+    default_title: "Click to open panel",
   },
+  options_ui: {
+    page: "src/options/index.html",
+  },
+  side_panel: {
+    default_path: "src/sidepanel/index.html",
+  },
+  background: {
+    service_worker: "src/background/index.ts",
+  },
+  permissions: ["storage", "sidePanel"],
 });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), crx({ manifest })]
-
-})
+  plugins: [
+    /*
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+          dest: ".",
+        },
+        {
+          src: "node_modules/bootstrap/dist/css/bootstrap.min.css",
+          dest: ".",
+        },
+      ],
+    }),
+    */
+    svelte(),
+    crx({ manifest }),
+  ],
+});
